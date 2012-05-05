e9738e28347ae324c54c54d40626869492897ed2
Kristian Kjems <kristian.kjems@gmail.com>
2012-05-05 16:13:21 +0200

    Fixed where a conflict in a meta file would not resolve

--- VersionControlVS/UnityVersionControl/Source/GUI/Utility/VCConflictHandler.cs
+++ VersionControlVS/UnityVersionControl/Source/GUI/Utility/VCConflictHandler.cs
@@ -17,7 +17,7 @@ namespace VersionControl
         private static readonly List<string> ignoredConflicts = new List<string>();
         private static void HandleConflicts()
         {
-            var conflicts = VCCommands.Instance.GetFilteredAssets((a, s) => s.fileStatus == VCFileStatus.Conflicted);
+            var conflicts = VCCommands.Instance.GetFilteredAssets((a, s) => s.fileStatus == VCFileStatus.Conflicted || s.MetaStatus().fileStatus == VCFileStatus.Conflicted);
             foreach (var conflictIt in conflicts)
             {
                 if (ignoredConflicts.Contains(conflictIt)) continue;
@@ -25,7 +25,7 @@ namespace VersionControl
                 int result = EditorUtility.DisplayDialogComplex("Conflict", "There is a conflict in the file '" + conflictIt + "'. Use 'Theirs' or 'Mine'?", "Theirs", "Mine", "Ignore");
                 if (result == 0 || result == 1)
                 {
-                    VCCommands.Instance.Resolve(new[] {conflictIt}, result == 0 ? ConflictResolution.Theirs : ConflictResolution.Mine);
+                    VCCommands.Instance.Resolve(new[] {conflictIt, conflictIt + ".meta"}, result == 0 ? ConflictResolution.Theirs : ConflictResolution.Mine);
                 }
                 else
                 {

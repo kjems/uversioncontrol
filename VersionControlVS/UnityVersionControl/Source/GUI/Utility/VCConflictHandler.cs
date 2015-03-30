a0a7d3f78525975ff46809306dd6a2c6f7b64a86
Kristian Kjems <kjems@playdeadgames.com>
2015-03-30 13:41:00 +0200

    More linefeeds to make text for readable

--- VersionControlVS/UnityVersionControl/Source/GUI/Utility/VCConflictHandler.cs
+++ VersionControlVS/UnityVersionControl/Source/GUI/Utility/VCConflictHandler.cs
@@ -25,7 +25,7 @@ namespace VersionControl
                     const string explanation = "\nTheirs :\nUse the file from the server and discard local changes to the file\n\nMine :\nUse my version of the file and discard the changes someone else made on the server";
                     const string mergeExplanation = "\nMerge External :\nIgnore the conflict in UVC and handle the conflict in an external program";
                     const string ignoreExplanation = "\nIgnore :\nIgnore the conflict for now although the file will not be readable by Unity";
-                    string message = string.Format("There is a conflict in the file '{0}'. Use 'Theirs' or 'Mine'?\n {1}\n{2}\n", conflictIt.Compose(), explanation, mergable ? mergeExplanation : ignoreExplanation);
+                    string message = string.Format("There is a conflict in the file:\n '{0}'\n\nUse 'Theirs' or 'Mine'?\n {1}\n{2}\n", conflictIt.Compose(), explanation, mergable ? mergeExplanation : ignoreExplanation);
                     int result = EditorUtility.DisplayDialogComplex("Conflict", message, "Theirs", "Mine", mergable ? "Merge External" : "Ignore");
                     if (result == 0 || result == 1)
                     {

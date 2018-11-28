47b69c3e54f43c2b3cf2961123dee36523fa74e4
Kristian Kjems <kristian@playdeadgames.com>
2018-11-28 18:36:03 +0100

    Adding search to branch window

--- UVC.UnityVersionControl/GUI/Windows/VCBranchWindow.cs
+++ UVC.UnityVersionControl/GUI/Windows/VCBranchWindow.cs
@@ -4,21 +4,25 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
+using UnityEditor.IMGUI.Controls;
 using UVC.Logging;
 
 namespace UVC.UserInterface
 {
     using MultiColumnState = MultiColumnState<BranchStatus, GUIContent>;
     using MultiColumnViewOption = MultiColumnView.MultiColumnViewOption<BranchStatus>;
-    
+
     internal class BranchWindow : EditorWindow
     {
         public static BranchWindow instance;
         static BranchMulticolumnList branchColumnList;
+        private static SearchField searchField;
 
         private static string currentBranch = "";
         private static string trunkpath = null;
         private static string branchpath = null;
+        private static string searchString;
+        private static List<BranchStatus> branches = new List<BranchStatus>();
         public static string BranchPath
         {
             get { return branchpath ?? (branchpath = EditorPrefs.GetString("BranchWindow/BranchPath")); }
@@ -28,7 +32,7 @@ namespace UVC.UserInterface
                 EditorPrefs.SetString("BranchWindow/BranchPath", branchpath);
             }
         }
-        
+
         public static void Create()
         {
             if (instance == null)
@@ -42,6 +46,7 @@ namespace UVC.UserInterface
 
         private void OnEnable()
         {
+            searchField = new SearchField();
             if (string.IsNullOrEmpty(branchpath)) branchpath = VCCommands.Instance.GetBranchDefaultPath();
             if (string.IsNullOrEmpty(trunkpath)) trunkpath = VCCommands.Instance.GetTrunkPath();
             branchColumnList = new BranchMulticolumnList();
@@ -54,7 +59,7 @@ namespace UVC.UserInterface
             VCCommands.Instance.OperationCompleted -= InstanceOnOperationCompleted;
             instance = null;
         }
-        
+
         private void InstanceOnOperationCompleted(OperationType operation, VersionControlStatus[] beforeStatus, VersionControlStatus[] afterStatus, bool success)
         {
             Refresh();
@@ -66,8 +71,8 @@ namespace UVC.UserInterface
             BranchToolbarGUI();
             EditorGUILayout.EndHorizontal();
             BranchListGUI();
-        }       
-        
+        }
+
         void BranchToolbarGUI()
         {
             if (GUILayout.Button("Refresh", EditorStyles.toolbarButton, GUILayout.Width(50)))
@@ -84,8 +89,8 @@ namespace UVC.UserInterface
             if (GUILayout.Button(Terminology.merge, EditorStyles.toolbarButton, GUILayout.Width(50)))
             {
                 var fromBranch = branchColumnList.GetSelection().First().name;
-                ConfirmMerge(GetChangedAssets().Any(), fromBranch, currentBranch, 
-                    mergeAction: () => 
+                ConfirmMerge(GetChangedAssets().Any(), fromBranch, currentBranch,
+                    mergeAction: () =>
                     {
                         Merge(fromBranch);
                         Refresh();
@@ -102,9 +107,15 @@ namespace UVC.UserInterface
                 newBranchWindow.toPath = BranchPath + DateTime.Now.ToString("yyyy-MM-dd_");
                 newBranchWindow.ShowUtility();
             }
-            
+
+            string newSearchString = searchField.OnToolbarGUI(searchString);
+            if (newSearchString != searchString)
+            {
+                searchString = newSearchString;
+                RefreshBranchList();
+            }
+
             GUILayout.FlexibleSpace();
-            
             GUILayout.Label("Branch Path:", EditorStyles.miniLabel, GUILayout.Width(70));
             string newBranchPath = GUILayout.TextField(BranchPath, EditorStyles.toolbarTextField, GUILayout.ExpandWidth(true), GUILayout.MinWidth(200));
             if (newBranchPath != BranchPath)
@@ -112,6 +123,12 @@ namespace UVC.UserInterface
                 BranchPath = newBranchPath;
             }
         }
+
+        private static void RefreshBranchList()
+        {
+            branchColumnList.SetBranches(string.IsNullOrEmpty(searchString) ? branches : branches.Where(s => s.name.Contains(searchString) || s.author.Contains(searchString)));
+        }
+
         private void ConfirmMerge(bool modifiedLocalCopy, string from, string to, Action mergeAction)
         {
             var mergeConfirmation = CreateInstance<MergeConfirmationWindow>();
@@ -126,7 +143,7 @@ namespace UVC.UserInterface
 
         static IEnumerable<VersionControlStatus> GetChangedAssets()
         {
-            return VCCommands.Instance.GetFilteredAssets(status => 
+            return VCCommands.Instance.GetFilteredAssets(status =>
                     status.fileStatus == VCFileStatus.Modified ||
                     status.fileStatus == VCFileStatus.Added ||
                     status.fileStatus == VCFileStatus.Conflicted ||
@@ -143,7 +160,7 @@ namespace UVC.UserInterface
             VCCommands.Instance.RequestAssetDatabaseRefresh();
             Refresh();
         }
-        
+
         static async void Merge(string url)
         {
             int progressCounter = 0;
@@ -151,7 +168,7 @@ namespace UVC.UserInterface
             {
                 EditorUtility.DisplayProgressBar("Merge", s, ((progressCounter++ % 25) / 25f) );
             }
-            
+
             VCCommands.Instance.ProgressInformation += UpdateMergeProgress;
             bool result = await VCCommands.Instance.MergeBranchTask(url);
             VCCommands.Instance.ProgressInformation -= UpdateMergeProgress;
@@ -162,13 +179,13 @@ namespace UVC.UserInterface
                 VCCommands.Instance.CommitDialog(GetChangedAssets().Select(status => status.assetPath.Compose()).ToList(), includeDependencies: false, showUserConfirmation: true, commitMessage: $"Merged {url} to {currentBranch}");
                 VCCommands.Instance.RequestAssetDatabaseRefresh();
             }
-        }       
+        }
         static async void Refresh()
         {
             if (!branchpath.EndsWith("/")) branchpath += "/";
             if (VCCommands.Active)
             {
-                var relativeBranches = await VCCommands.Instance.RemoteListTask(BranchPath);
+                branches = await VCCommands.Instance.RemoteListTask(BranchPath);
                 var trunkInfo = VCCommands.Instance.GetInfo(trunkpath);
                 var trunk = new BranchStatus()
                 {
@@ -177,11 +194,11 @@ namespace UVC.UserInterface
                     date = trunkInfo.lastChangedDate,
                     revision = trunkInfo.revision
                 };
-                relativeBranches.Insert(0, trunk);
-                branchColumnList.SetBranches(relativeBranches);
+                branches.Insert(0, trunk);
+                RefreshBranchList();
                 currentBranch = await VCCommands.Instance.GetCurrentBranchTask();
                 await VCCommands.Instance.StatusTask(StatusLevel.Previous, DetailLevel.Normal);
-                //Repaint();
+                instance.Repaint();
             }
         }
 
@@ -189,7 +206,7 @@ namespace UVC.UserInterface
         {
             branchColumnList.DrawGUI();
         }
-        
+
         internal class BranchMulticolumnList
         {
             private MultiColumnState         multiColumnState;
@@ -202,31 +219,31 @@ namespace UVC.UserInterface
 
             private GUIContent activeBranch = new GUIContent("  ▶", "Active Branch");
             private GUIContent emptyGUIContent = new GUIContent();
-            
+
             public BranchMulticolumnList()
             {
                 Initialize();
             }
-    
+
             private void Initialize()
             {
-                
+
                 columnActive = new MultiColumnState.Column(new GUIContent("", "Active Branch"), data => currentBranch == data.name ?  activeBranch : emptyGUIContent);
                 columnPath = new MultiColumnState.Column(new GUIContent("Path"), data => new GUIContent(data.name));
                 columnAuthor = new MultiColumnState.Column(new GUIContent("Author"), data => new GUIContent(data.author));
                 columnRevision = new MultiColumnState.Column(new GUIContent("Revision"), data => new GUIContent(data.revision.ToString()));
                 columnDate = new MultiColumnState.Column(new GUIContent("Date"), data => new GUIContent(data.date.ToString("yyyy-MM-dd HH:mm:ss")));
                 multiColumnState = new MultiColumnState();
-                
+
                 Func<MultiColumnState.Row, MultiColumnState.Column, bool> cellClickAction = (row, column) =>
                 {
                     GUI.FocusControl("");
                     DebugLog.Log(row.data + " : "  + column.GetContent(row.data).text);
                     return false;
                 };
-                
+
                 var guiSkin = EditorGUIUtility.GetBuiltinSkin( EditorGUIUtility.isProSkin ? EditorSkin.Scene : EditorSkin.Inspector);
-    
+
                 Func<MultiColumnState.Row, MultiColumnState.Column, GenericMenu> rowRightClickMenu = (row, column) =>
                 {
                     GenericMenu menu = new GenericMenu();
@@ -234,14 +251,14 @@ namespace UVC.UserInterface
                     menu.AddItem(new GUIContent("Merge"), false, () => Merge(row.data.name));
                     return menu;
                 };
-                
+
                 Func<MultiColumnState.Column, GenericMenu> headerRightClickMenu = column =>
                 {
                     GenericMenu menu = new GenericMenu();
                     return menu;
                 };
-                
-                
+
+
                 options = new MultiColumnViewOption
                 {
                     headerStyle = new GUIStyle(guiSkin.button),
@@ -255,40 +272,40 @@ namespace UVC.UserInterface
                         //DebugLog.Log(path.name);
                     }
                 };
-    
+
                 options.headerStyle.fixedHeight = 20.0f;
                 options.rowStyle.onNormal.background = IconUtils.CreateSquareTexture(4, 1, new Color(0.24f, 0.5f, 0.87f, 0.75f));
                 options.rowStyle.margin = new RectOffset(2, 2, 2, 1);
                 options.rowStyle.border = new RectOffset(0, 0, 0, 0);
                 options.rowStyle.padding = new RectOffset(0, 0, 0, 0);
-                
+
                 multiColumnState.AddColumn(columnActive);
                 options.widthTable.Add(columnActive.GetHeader().text, 25);
-                
+
                 multiColumnState.AddColumn(columnPath);
                 options.widthTable.Add(columnPath.GetHeader().text, 350);
-                
+
                 multiColumnState.AddColumn(columnAuthor);
                 options.widthTable.Add(columnAuthor.GetHeader().text, 90);
-                
+
                 multiColumnState.AddColumn(columnRevision);
                 options.widthTable.Add(columnRevision.GetHeader().text, 80);
-                
+
                 multiColumnState.AddColumn(columnDate);
                 options.widthTable.Add(columnDate.GetHeader().text, 150);
-                
+
             }
-    
+
             public void SetBranches(IEnumerable<BranchStatus> branches)
             {
                 multiColumnState.Refresh(branches);
             }
-            
+
             public IEnumerable<BranchStatus> GetSelection()
             {
                 return multiColumnState.GetSelected();
             }
-    
+
             public void DrawGUI()
             {
                 Rect rect = GUILayoutUtility.GetRect(5, float.MaxValue, 5, float.MaxValue, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
@@ -299,5 +316,5 @@ namespace UVC.UserInterface
 
     }
 
-    
+
 }

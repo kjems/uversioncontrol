56a9803a2ef611a601e00afe1ff70f2108bae15d
Kristian Kjems <kristian@playdead.com>
2019-09-11 11:08:15 +0200

    Shortcuts unassigned as default

--- UVC.UnityVersionControl/GUI/Menus/Shortcuts.cs
+++ UVC.UnityVersionControl/GUI/Menus/Shortcuts.cs
@@ -7,19 +7,19 @@ namespace UVC
 {
     public class Shortcuts
     {
-        [Shortcut("UVC/" + Terminology.getlock, null, KeyCode.Q, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
+        [Shortcut("UVC/" + Terminology.getlock, null)]
         static void GetLock()
         {
             VCCommands.Instance.GetLockTask(new[] {VCSceneViewGUI.currentContext()});
         }
         
-        [Shortcut("UVC/" + Terminology.allowLocalEdit, null, KeyCode.W, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
+        [Shortcut("UVC/" + Terminology.allowLocalEdit, null)]
         static void AllowLocalEdit()
         {
             VCCommands.Instance.AllowLocalEditTask(new[] {VCSceneViewGUI.currentContext()});
         }
         
-        [Shortcut("UVC/" + Terminology.revert, null, KeyCode.E, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
+        [Shortcut("UVC/" + Terminology.revert, null)]
         static void Revert()
         {
             VCCommands.Instance.RevertTask(new[] {VCSceneViewGUI.currentContext()});

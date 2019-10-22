37cb3b8750f15e8eddbaf3177d272343596ceec5
Kristian Kjems <kristian@playdead.com>
2019-10-22 16:42:11 +0200

    Fixed race condition in profiler markers

--- UVC.UnityVersionControl/API/ProfilerScope.cs
+++ UVC.UnityVersionControl/API/ProfilerScope.cs
@@ -1,17 +1,17 @@
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using Unity.Profiling;
 
 namespace UVC
 {
     public static class ProfilerScope
     {
-        static readonly Dictionary<string, ProfilerMarker> markerCache = new Dictionary<string, ProfilerMarker>();
+        static readonly ConcurrentDictionary<string, ProfilerMarker> markerCache = new ConcurrentDictionary<string, ProfilerMarker>();
         public static ProfilerMarker.AutoScope Get(string name)
         {
             if (!markerCache.TryGetValue(name, out var marker))
             {
                 marker = new ProfilerMarker(name);
-                markerCache.Add(name, marker);
+                markerCache[name] = marker;
             }
             return marker.Auto();
         }
